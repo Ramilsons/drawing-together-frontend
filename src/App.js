53de8423f479dfda7928cdc0798577c43232f7c7
Ramilsons <ramilsonfelixsilva9@gmail.com>
2024-01-05 14:19:23 -0300

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import sketch from './sketch';
+import { ReactP5Wrapper } from 'react-p5-wrapper';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-black">
+      <ReactP5Wrapper sketch={sketch} />
     </div>
   );
 }
